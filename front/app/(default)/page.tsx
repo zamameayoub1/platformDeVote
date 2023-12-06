fc753cdf9d5acf711ee7b171866910b4614132de
zamameayoub1 <143510556+zamameayoub1@users.noreply.github.com>
2023-12-06 23:08:50 +0100

    new-front

--- front/app/(default)/page.tsx
+++ front/app/(default)/page.tsx
@@ -4,19 +4,13 @@ export const metadata = {
 }
 
 import Hero from '@/components/hero'
-import Features from '@/components/features'
-import Newsletter from '@/components/newsletter'
-import Zigzag from '@/components/zigzag'
 import Testimonials from '@/components/testimonials'
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Features />
-      <Zigzag />
       <Testimonials />
-      <Newsletter />
     </>
   )
 }
